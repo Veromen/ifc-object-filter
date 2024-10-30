85005afdbc632c6c628bdcc793e663be394e8009
Simen Horne <simenhorne@hotmail.com>
2024-10-30 14:10:51 +0100

    Alt saman

--- custom_importer.py
+++ custom_importer.py
@@ -3,7 +3,7 @@ import os
 import importlib.util
 
 def import_local_ifcopenshell():
-    local_ifcopenshell_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'local_ifcopenshell'))
+    local_ifcopenshell_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'ifcopenshell'))
     ifcopenshell_spec = importlib.util.spec_from_file_location("ifcopenshell", os.path.join(local_ifcopenshell_path, "__init__.py"))
     ifcopenshell = importlib.util.module_from_spec(ifcopenshell_spec)
     sys.modules["ifcopenshell"] = ifcopenshell
